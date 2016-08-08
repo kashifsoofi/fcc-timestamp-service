ba4d067460af7a3fc1f8669fb84ade8a62623385
Kashif Jamal Soofi <kashif.soofi+c9@gmail.com>
2016-08-08 08:56:00 +0000

    Timestamp microservice implementation and index page

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-var express = require("express")
+var express = require("express");
+var moment = require("moment");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html');
+});
+
+app.get('/:date', function (req, res) {
+  var format = "";
+  if (req.params.date === parseInt(req.params.date, 10)) {
+    format = "X";
+  }
+  else {
+    format = "MMMM D, YYYY";
+  }
+  var date = moment(req.params.date, format);
+  
+  if (date.isValid()) {
+    res.json({
+      unix: date.format("X"),
+      natural: date.format("MMM D, YYYY")
+    });
+  }
+  else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
 });
 
 app.listen(8080, function () {
