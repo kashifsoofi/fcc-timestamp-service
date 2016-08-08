4c915f427a34afe99e020556c62e8c3122330d88
Kashif Jamal Soofi <kashif.soofi+c9@gmail.com>
2016-08-08 10:04:43 +0000

    Add ejs view engine to render index

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require("express");
+var path = require("path");
 var moment = require("moment");
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  //res.sendFile('index.html');
+  var appUrl = req.protocol + '://' + req.get('host') + '/';
+  res.render('index', { appUrl: appUrl });
 });
 
 app.get('/:date', function (req, res) {
